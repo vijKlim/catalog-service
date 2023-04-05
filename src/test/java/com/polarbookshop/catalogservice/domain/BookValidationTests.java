141394d323070cd012e56a5b1d6b48cc6c16a036
Vitalii Klim <vij.klimenko@gmail.com>
2023-04-05 23:06:57 +0300

    ch5

--- src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
@@ -22,7 +22,7 @@ public class BookValidationTests {
     @Test
     void whenAllFieldsCorrectThenValidationSuccess() {
         var book =
-            new Book("1234567890", "Title", "Author", 9.90);
+            Book.of("1234567890", "Title", "Author", 9.90);
         Set<ConstraintViolation<Book>> violations =  validator.validate(book);
         assertThat(violations).isEmpty();
     }
@@ -30,7 +30,7 @@ public class BookValidationTests {
     @Test
     void whenIsbnDefinedButIncorrectThenValidationFails() {
         var book =
-            new Book("a234567890", "Title", "Author", 9.90);
+            Book.of("a234567890", "Title", "Author", 9.90);
         Set<ConstraintViolation<Book>> violations = validator.validate(book);
         assertThat(violations).hasSize(1);
         assertThat(violations.iterator().next().getMessage())
